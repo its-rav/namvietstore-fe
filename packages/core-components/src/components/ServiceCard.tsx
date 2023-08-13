978e0dc791a029fe01cc7e44d8010abb2730c01a
phongduong-dp <duonghongphong-dp@gmail.com>
2023-08-13 19:27:59 +0700

    chore: modify service card

--- packages/core-components/src/components/ServiceCard.tsx
+++ packages/core-components/src/components/ServiceCard.tsx
@@ -21,19 +21,19 @@ const ServiceCard: React.FC<ServiceCardProps> = ({
 }) => {
   return (
     <div
-      className={`flex flex-col items-center justify-center rounded-md space-y-3 border-2 md:text-base sm:text-xs border-gray-200 md:w-56 md:h-52 sm:w-40 sm:h-40 font-primary text-center px-2 py-2 bg-white ${className}`}
+      className={`flex flex-col items-center font-normal justify-center rounded-md space-y-3 border-2 md:text-base sm:text-xs border-gray-200 md:w-56 md:h-52 sm:w-36 sm:h-36 font-primary my-2 mx-2   text-center px-3 py-1 bg-white ${className}`}
     >
       {serviceCardItem && (
         <>
-          <div className='md:w-14 md:h-14 sm:w-10 sm:h-10'>
+          <div className='md:w-14 md:h-14 sm:w-9 sm:h-9'>
             {serviceCardItem.icon}
           </div>
           <p
-            className={`mt-3 md:text-xl font-bold md:leading-6 sm:text-sm text-primary ${titleClassName}`}
+            className={`mt-3 h-6 md:text-xl font-bold md:leading-6 sm:text-sm text-primary ${titleClassName}`}
           >
             {serviceCardItem.title}
           </p>
-          <p className={`${descriptionClassName}`}>
+          <p className={`${descriptionClassName} h-10 mt-2`}>
             {serviceCardItem.description}
           </p>
         </>
