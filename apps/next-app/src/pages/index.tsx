2accb72ebb7cd308e08a0b9504b3493b64375e6c
rav <vonhan315@gmail.com>
2023-08-20 12:44:36 +0700

    chore: move next-app to apps

--- apps/next-app/src/pages/index.tsx
+++ apps/next-app/src/pages/index.tsx
@@ -4,24 +4,24 @@ import { Button, DropdownButton, StatisticSection } from "@namviet-fe/core-ui";
 export default function Home() {
   return (
     <HomeLayout>
-      <Button variant="default" size="base" content="Đọc thêm" />
+      <Button>Click me</Button>
       <StatisticSection
-        content={[
+        statisticItems={[
           {
             title: "mặt hàng",
-            statistic: "500",
+            statistic: 500,
           },
           {
             title: "khách hàng hài lòng",
-            statistic: "100",
+            statistic: 100,
           },
           {
             title: "dự án thành công",
-            statistic: "200",
+            statistic: 200,
           },
           {
             title: "thành lập doanh nghiệp",
-            statistic: "2010",
+            statistic: 2010,
           },
         ]}
       />
