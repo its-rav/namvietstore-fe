7517dce53ccbe85f62dfdedf20c32bc9c61b23e2
Neeze <phanvanhainam.2019@gmail.com>
2023-08-25 16:04:23 +0700

    add feature topbar

--- packages/core-components/src/components/TopBar.tsx
+++ packages/core-components/src/components/TopBar.tsx
@@ -16,39 +16,36 @@ const TopBar: React.FC<TopBarProps> = ({
     linkContact2,
 }) => {
     return (
-        <div className={`${className} container bg-primary flex flex-row items-center`}>
-            {contactIcon &&
-                <div className = {
-                    "ContactIcon md:basis-8 " +
-                    "md:ms-8 md:my-3 " +
-                    "sm:basis-6 sm:ms-6 sm:my-2 "
-                }>
-                    {contactIcon}
-                </div>
-            }
+        <div className={`${className} container bg-primary flex flex-row items-center w-screen`}>
+            <div className = {"ContactIcon basis-8 shrink-0" +
+                "visible sm:invisible " +
+                "md:visible  md:ms-12 md:ml-2 "}>
+                {contactIcon}
+            </div>
             <div className = {
-                "ContactInfo-1 sm:flex-2 sm:font-Roboto sm:text-slate-200 border-e-2 " +
-                "md:text-sm " +
-                "sm:text-xs sm:px-4 " +
+                "ContactInfo-1 font-Roboto text-slate-200 border-e-2 font-medium " +
+                "sm:px-4 sm:text-xs" +
+                "md:text-sm lg:text-lg " +
                 "hover:text-white " +
                 "hover:drop-shadow-2xl "
             }>
                 <a href={linkContact1}><span className={"sm:hidden md:contents"}>+91 9876543210 - </span>Mr. Duong</a>
             </div>
             <div className = {
-                "ContactInfo-2 flex-3 font-Roboto text-slate-200 " +
-                "md:text-sm  " +
-                "sm:text-xs sm:px-4 " +
+                "ContactInfo-2 font-Roboto text-slate-200 font-medium " +
+                "sm:px-4 sm:text-xs" +
+                "md:text-sm lg:text-lg " +
                 "hover:text-white " +
                 "hover:drop-shadow-2xl "
             }>
                 <a href={linkContact2}><span className={"sm:hidden md:contents "}>+91 9876543210 - </span>Ms. Huynh</a>
             </div>
-            <div className = {"LanguageButon flex-4 " +
-                "sm:ml-auto md:me-8 sm:me-3"} >
+            <div className = {"LanguageButon scale-[95%] py-3  " +
+                "ml-auto md:me-8"} >
                 {languageButton}
             </div>
         </div>
     )
 }
+
 export default TopBar
--- playground/src/topbar/TopBar.stories.tsx
+++ playground/src/topbar/TopBar.stories.tsx
@@ -6,18 +6,38 @@ import "@namviet-fe/core-ui/dist/style.css";
 import {TopBar, type TopBarProps } from "@namviet-fe/core-ui";
 import {ContactIcon} from "@namviet-fe/core-ui";
 import {Button, type ButtonProps} from "@namviet-fe/core-ui";
-
+import { DropdownButton, type DropdownButtonProps } from "@namviet-fe/core-ui";
+import { FaFlagUsa } from "react-icons/fa";
+import { GiUsaFlag } from "react-icons/gi";
+import { EnIcon, ViIcon } from "@namviet-fe/core-ui";
+import {LanguageSelect} from "../buttons/Select.stories";
 export default {
 title: "@nv-fe/core-ui/topbar/TopBar",
 } satisfies StoryDefault<typeof TopBar>;
 
-export let Default: Story;
-Default = () => (
+export let Default: Story<DropdownButtonProps>;
+Default = ({ selected, options }) => (
     <TopBar
         className="TopBar"
-        contactIcon={<ContactIcon/>}
-        languageButton = {<Button children="Đọc thêm" isLoading={true} />}
+        contactIcon={<ContactIcon />}
+        languageButton = {<DropdownButton selected={selected} options={options} />}
         linkContact1={""}
         linkContact2={""}
     ></TopBar>
 );
+
+Default.defaultProps = {
+    selected: "vn",
+    options: [
+        {
+            key: "en",
+            label: "English",
+            icon: <EnIcon />,
+        },
+        {
+            key: "vn",
+            label: "Tiếng Việt",
+            icon: <ViIcon />,
+        },
+    ],
+};
