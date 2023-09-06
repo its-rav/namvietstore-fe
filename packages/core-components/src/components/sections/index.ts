05326c9858b6e04fcf37a35e80998a9ec8f2bd4e
Phong Duong <92157056+phongduong-dp@users.noreply.github.com>
2023-09-06 20:55:26 +0700

    feat: intro-panel (#18)
    * feat: intro panel component and story
    
    * chore: pick up rav comments
    
    ---------
    
    Co-authored-by: phongduong-dp <duonghongphong-dp@gmail.com>

--- packages/core-components/src/components/sections/index.ts
+++ packages/core-components/src/components/sections/index.ts
@@ -1,3 +1,6 @@
+import IntroPanel, {
+  type IntroPanelItemType,
+} from '@/components/sections/IntroPanel';
 import MarqueeSection, {
   type MarqueeItemType,
 } from '@/components/sections/MarqueeSection';
@@ -7,9 +10,11 @@ import StatisticSection, {
 } from '@/components/sections/StatisticSection';
 
 export {
+  type IntroPanelItemType,
   type MarqueeItemType,
   type StatisticItemType,
   type StatisticSectionProps,
+  IntroPanel,
   MarqueeSection,
   StatisticSection,
 };
