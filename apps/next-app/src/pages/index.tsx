2621c642455a4a39fc50d1c1a6fe6c44c5a022ff
rav <vonhan315@gmail.com>
2023-09-09 21:50:41 +0700

    chore: add layouts, wrappers

--- apps/next-app/src/pages/index.tsx
+++ apps/next-app/src/pages/index.tsx
@@ -1,30 +1,8 @@
-import Image from "next/image";
 import HomeLayout from "@/components/layouts/home-layout";
-import { Button, DropdownButton, StatisticSection } from "@namviet-fe/core-ui";
 export default function Home() {
   return (
     <HomeLayout>
-      <Button>Click me</Button>
-      <StatisticSection
-        statisticItems={[
-          {
-            title: "mặt hàng",
-            statistic: 500,
-          },
-          {
-            title: "khách hàng hài lòng",
-            statistic: 100,
-          },
-          {
-            title: "dự án thành công",
-            statistic: 200,
-          },
-          {
-            title: "thành lập doanh nghiệp",
-            statistic: 2010,
-          },
-        ]}
-      />
+      <div></div>
     </HomeLayout>
   );
 }
