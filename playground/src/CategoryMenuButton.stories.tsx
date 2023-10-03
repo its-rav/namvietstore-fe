c987fef68f61d6b8ba1173eda890bd3360aafd98
Neeze <60569049+Neeze@users.noreply.github.com>
2023-10-03 13:23:04 +0700

    Revert "Delete playground directory"
    This reverts commit c0b75a591f9983dd1f5f85bd634893872c137884.

---
+++ playground/src/CategoryMenuButton.stories.tsx
@@ -0,0 +1,222 @@
+import type { Story, StoryDefault } from "@ladle/react";
+import React from "react";
+import "regenerator-runtime/runtime";
+import "@namviet-fe/core-ui/dist/style.css";
+
+import { type CategoryProps, CategoryMenuButton } from "@namviet-fe/core-ui";
+import { StripsIcon } from "@namviet-fe/core-ui";
+import { KeyIcon } from "@namviet-fe/core-ui";
+
+export default {
+  title: "@nv-fe/core-ui/CategoryMenuButton",
+} satisfies StoryDefault<typeof CategoryMenuButton>;
+
+export const Default: Story<CategoryProps> = ({
+  className,
+  children,
+  icon,
+  directory,
+}) => (
+  <div
+    style={{
+      display: "block",
+      marginLeft: "20px",
+      marginRight: "20px",
+      marginTop: "40px",
+    }}
+  >
+    <CategoryMenuButton
+      className={className}
+      children={children}
+      icon={icon}
+      directory={directory}
+    />
+  </div>
+);
+
+Default.defaultProps = {
+  className: "CategoryButton",
+  children: "DANH MỤC SẢN PHẨM",
+  icon: <StripsIcon />,
+  directory: [
+    {
+      id: 0,
+      icon: <KeyIcon />,
+      rootName: "ĐẦU ĐỐT",
+      rootLink: "",
+      childrenDirectory: [
+        {
+          folderTitle: "Đầu đốt theo hãng",
+          folderItems: [
+            {
+              itemTitle: "Madas",
+              itemLink: "",
+            },
+            {
+              itemTitle: "Blowtherm",
+              itemLink: "",
+            },
+            {
+              itemTitle: "Monarc",
+              itemLink: "",
+            },
+          ],
+        },
+        {
+          folderTitle: "Đầu đốt theo loại nhiên liệu",
+          folderItems: [
+            {
+              itemTitle: "Đầu đốt gas",
+              itemLink: "",
+            },
+            {
+              itemTitle: "Đầu đốt DO",
+              itemLink: "",
+            },
+            {
+              itemTitle: "Đầu đốt FO",
+              itemLink: "",
+            },
+          ],
+        },
+        {
+          folderTitle: "Đầu đốt theo lĩnh vực",
+          folderItems: [
+            {
+              itemTitle: "Bê tông nhựa nóng",
+              itemLink: "",
+            },
+            {
+              itemTitle: "Sản xuất",
+              itemLink: "",
+            },
+            {
+              itemTitle: "Trạm trộn",
+              itemLink: "",
+            },
+          ],
+        },
+        {
+          folderTitle: "Đầu đốt theo lĩnh vực",
+          folderItems: [
+            {
+              itemTitle: "Bê tông nhựa nóng",
+              itemLink: "",
+            },
+            {
+              itemTitle: "Sản xuất",
+              itemLink: "",
+            },
+            {
+              itemTitle: "Trạm trộn",
+              itemLink: "",
+            },
+          ],
+        },
+        {
+          folderTitle: "Đầu đốt theo lĩnh vực",
+          folderItems: [
+            {
+              itemTitle: "Bê tông nhựa nóng",
+              itemLink: "",
+            },
+            {
+              itemTitle: "Sản xuất",
+              itemLink: "",
+            },
+            {
+              itemTitle: "Trạm trộn",
+              itemLink: "",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      id: 1,
+      icon: <KeyIcon />,
+      rootName: "LAPTOP1",
+      rootLink: "",
+      childrenDirectory: [
+        {
+          folderTitle: "Cơ Khí1",
+          folderItems: [
+            {
+              itemTitle: "Bút chì",
+              itemLink: "",
+            },
+            {
+              itemTitle: "Bút chì",
+              itemLink: "",
+            },
+            {
+              itemTitle: "Bút chì",
+              itemLink: "",
+            },
+          ],
+        },
+        {
+          folderTitle: "Cơ Khí1",
+          folderItems: [
+            {
+              itemTitle: "Bút chì",
+              itemLink: "",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      id: 2,
+      icon: <KeyIcon />,
+      rootName: "LAPTOP2",
+      rootLink: "",
+      childrenDirectory: [
+        {
+          folderTitle: "Cơ Khí2",
+          folderItems: [
+            {
+              itemTitle: "Bút chì",
+              itemLink: "",
+            },
+          ],
+        },
+        {
+          folderTitle: "Cơ Khí2",
+          folderItems: [
+            {
+              itemTitle: "Bút chì",
+              itemLink: "",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      id: 3,
+      icon: <KeyIcon />,
+      rootName: "LAPTOP3",
+      rootLink: "",
+      childrenDirectory: [
+        {
+          folderTitle: "Cơ Khí3",
+          folderItems: [
+            {
+              itemTitle: "Bút chì",
+              itemLink: "",
+            },
+          ],
+        },
+        {
+          folderTitle: "Cơ Khí3",
+          folderItems: [
+            {
+              itemTitle: "Bút chì",
+              itemLink: "",
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
