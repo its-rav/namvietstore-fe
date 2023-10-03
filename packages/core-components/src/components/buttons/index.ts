fa6a0f3a59d9d15267d761fd64a4a0d9bf9bfc9a
Neeze <60569049+Neeze@users.noreply.github.com>
2023-10-03 15:17:57 +0700

    Delete Old Category Button Componnent

--- packages/core-components/src/components/buttons/index.ts
+++ packages/core-components/src/components/buttons/index.ts
@@ -4,8 +4,3 @@ import DropdownButton, {
 } from '@/components/buttons/DropdownButton';
 
 export { type ButtonProps, Button, DropdownButton, DropdownButtonProps };
-
-import CategoryMenuButton, {
-  type CategoryProps,
-} from '@/components/buttons/CategoryMenuButton';
-export { type CategoryProps, CategoryMenuButton };
