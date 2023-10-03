5adc30ddfda4f6a7d475fe02063dd363a8e2947d
phongduong-dp <duonghongphong-dp@gmail.com>
2023-10-03 21:35:32 +0700

    feat: contact table

---
+++ packages/core-components/src/components/ContactTable.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+
+export type ContactTableItemType = {
+  title: string;
+  description: string;
+  adress: string;
+  phone: string;
+  email: string;
+};
+
+type ContactTableProps = {
+  contactTableItem: ContactTableItemType;
+};
+
+const ContactTable: React.FC<ContactTableProps> = ({ contactTableItem }) => {
+  const titleFirstLine = contactTableItem.title
+    .split(' ')
+    .slice(0, 3)
+    .join(' ');
+  const titleSecondLine = contactTableItem.title.split(' ').slice(3).join(' ');
+
+  return (
+    <div className='w-96 font-primary leading-normal font-normal text-base text-white'>
+      <h2 className='text-xl font-bold uppercase'>
+        {titleFirstLine} <br /> {titleSecondLine}
+      </h2>
+      <div className='bg-white w-full h-1 my-4'>
+        <div className='bg-primary w-1/5 h-1'></div>
+      </div>
+      <p className='text-justify'>{contactTableItem.description}</p>
+      <h3 className='text-lg my-4'>Liên hệ</h3>
+      <div className='bg-white w-full h-1 my-4'>
+        <div className='bg-primary w-1/5 h-1'></div>
+      </div>
+      <div className='grid grid-cols-6 gap-3 w-11/12'>
+        <p className='col-span-2'>Địa chỉ</p>
+        <p className='col-span-4 text-justify'>{contactTableItem.adress}</p>
+        <p className='col-span-2'>Điện thoại</p>
+        <a
+          className='col-span-4 text-justify'
+          href={`tel: ${contactTableItem.phone}`}
+        >
+          {contactTableItem.phone}
+        </a>
+        <p className='col-span-2'>Email</p>
+        <a
+          className='col-span-4 text-justify'
+          href={`mailto: ${contactTableItem.email}`}
+        >
+          {contactTableItem.email}
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default ContactTable;
---
+++ playground/src/ContactTable.stories.tsx
@@ -0,0 +1,25 @@
+import type { Story, StoryDefault } from "@ladle/react";
+import React from "react";
+
+import { ContactTable, type ContactTableItemType } from "@namviet-fe/core-ui";
+import "@namviet-fe/core-ui/dist/style.css";
+
+export default {
+  title: "@nv-fe/core-ui/ContactTable",
+} satisfies StoryDefault<typeof ContactTable>;
+
+const contactTableItem: ContactTableItemType = {
+  title: "CÔNG TY TNHH THIẾT BỊ NHIỆT NAM VIỆT",
+  description:
+    "Công ty TNHH Thiết bị nhiệt Nam Việt GCNDKKD: 03456789 do Sở Kế hoạch và Đầu tư Thành phố Hồ Chí Minh cấp ngày 16/10/2018 ( Đăng ký thay đổi lần thứ 1 ngày 17/1/2023)",
+  adress:
+    "Phòng 21 Lầu 14, Tháp 1, Toà nhà Saigon Centre, 65 Lê lợi, Phường Bến Nghé, Quận 1, Tp.HCM, Việt Nam",
+  phone: "+ 84 7646578696",
+  email: "thaiduong.huynh@gmail.com",
+};
+
+export const Default: Story = () => (
+  <div style={{ padding: "20px", backgroundColor: "#002651" }}>
+    <ContactTable contactTableItem={contactTableItem} />
+  </div>
+);
