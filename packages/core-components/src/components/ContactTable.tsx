d07c3b7637da2569ce81060720b6dbf6cd7edf04
phongduong-dp <duonghongphong-dp@gmail.com>
2023-10-07 10:06:56 +0700

    chore: modify isNotNull function

--- packages/core-components/src/components/ContactTable.tsx
+++ packages/core-components/src/components/ContactTable.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import isNotNull from '@/utils/isNotNull';
+
 export type ContactTableItemType = {
   title?: string;
   subTitle?: string;
@@ -15,26 +17,18 @@ type ContactTableProps = {
 };
 
 const ContactTable: React.FC<ContactTableProps> = ({ contactTableItem }) => {
-  const isNull = (text: string | undefined) => {
-    if (text != null && text !== undefined) {
-      return true;
-    } else {
-      return false;
-    }
-  };
-
   return (
     <div className='md:w-96 font-primary leading-normal font-normal md:text-base text-white sm:text-sm sm:w-full'>
-      <h2 className='md:text-xl font-bold uppercase sm:text-base'>
-        {isNull(contactTableItem.subTitle) ? (
-          <>
-            {contactTableItem.subTitle} <br />
-          </>
-        ) : (
-          ''
-        )}
-        {isNull(contactTableItem.title) ? contactTableItem.title : ''}
-      </h2>
+      {isNotNull(contactTableItem.subTitle) && (
+        <h2 className='md:text-xl font-bold uppercase sm:text-base'>
+          {contactTableItem.subTitle}
+        </h2>
+      )}
+      {isNotNull(contactTableItem.title) && (
+        <h2 className='md:text-xl font-bold uppercase sm:text-base'>
+          {contactTableItem.title}
+        </h2>
+      )}
       <div className=' flex items-center bg-white w-full h-1 my-4'>
         <div className='bg-primary w-1/5 h-2'></div>
       </div>
@@ -50,13 +44,11 @@ const ContactTable: React.FC<ContactTableProps> = ({ contactTableItem }) => {
         <p className='col-span-4 text-justify'>{contactTableItem.phone}</p>
         <p className='col-span-2'>Email</p>
         <p className='col-span-4 text-justify'>{contactTableItem.email}</p>
+        <div className='col-span-2 mt-7'></div>
         {contactTableItem.icon && (
-          <>
-            <div className='col-span-2 mt-7'></div>
-            <div className='col-span-4 mt-7 w-52 h-20 object-cover'>
-              {contactTableItem.icon}
-            </div>
-          </>
+          <div className='col-span-4 mt-7 w-52 h-20 object-cover'>
+            {contactTableItem.icon}
+          </div>
         )}
       </div>
     </div>
