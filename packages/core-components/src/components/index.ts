19d73d21b7da9c52c2f1005e48e3fcd8136caaaf
phongduong-dp <duonghongphong-dp@gmail.com>
2023-10-10 22:47:39 +0700

    feat: add search icon

--- packages/core-components/src/components/index.ts
+++ packages/core-components/src/components/index.ts
@@ -1,11 +1,12 @@
-import PostCard from '@/components/PostCard';
-
-export { PostCard };
 import FeaturesCard, {
   type FeaturesCardItemType,
 } from '@/components/FeaturesCard';
+import PostCard from '@/components/PostCard';
+import SearchBar from '@/components/SearchBar';
 
+export { PostCard };
 export { type FeaturesCardItemType, FeaturesCard };
+export { SearchBar };
 
 export * from './buttons';
 export * from './sections';
