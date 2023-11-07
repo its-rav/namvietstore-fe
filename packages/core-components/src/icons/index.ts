af6b616a2a2ecdbc46222471cfc774c66aed45bc
phongduong-dp <duonghongphong.dp@gmail.com>
2023-11-07 23:04:47 +0700

    feat: add hamburger icon

--- packages/core-components/src/icons/index.ts
+++ packages/core-components/src/icons/index.ts
@@ -1,4 +1,5 @@
 export { ReactComponent as EnIcon } from './enflag.svg';
+export { ReactComponent as HamburgerIcon } from './hamburger.svg';
 export { ReactComponent as HeadphonesIcon } from './headphones.svg';
 export { ReactComponent as KeyIcon } from './key.svg';
 export { ReactComponent as PlusIcon } from './plus.svg';
