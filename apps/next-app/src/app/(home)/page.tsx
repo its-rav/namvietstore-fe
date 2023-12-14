ec1041d584b7a9b08d9e80ff35df93b05d8dc5e9
phongduong-dp <duonghongphong.dp@gmail.com>
2023-12-14 21:24:16 +0700

    chore: prettier

--- apps/next-app/src/app/(home)/page.tsx
+++ apps/next-app/src/app/(home)/page.tsx
@@ -3,16 +3,15 @@
 import { Button } from '@namviet-fe/core-ui';
 import { useRouter } from 'next/navigation';
 
-
 export default function Home() {
   const router = useRouter();
   return (
-      <Button
-        onClick={() => {
-          router.push('/products');
-        }}
-      >
-        Product Page
-      </Button>
+    <Button
+      onClick={() => {
+        router.push('/products');
+      }}
+    >
+      Product Page
+    </Button>
   );
 }
