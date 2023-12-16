045d8934c03857648943599e13bda5872667fa76
cuong.do <quoccuonguit.1998@mail.com>
2023-12-16 16:35:15 +0700

    add function submit all data of form

--- packages/core-components/src/components/contact/ContactMap.tsx
+++ packages/core-components/src/components/contact/ContactMap.tsx
@@ -12,7 +12,6 @@ const ContactMap: React.FC<ContactMapProps> = ({ contactMapItem }) => {
   return contactMapItem?.srcMap ? (
     <div className=''>
       <iframe
-        // src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7560.800330218261!2d106.6943383706771!3d10.785106391105177!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752f4b3330bcc7%3A0x4db964d76bf6e18e!2zVGjhuqNvIEPhuqdtIFZpw6puIFPDoGkgR8Oybg!5e0!3m2!1svi!2s!4v1702550513565!5m2!1svi!2s'
         src={contactMapItem.srcMap}
         width='100%'
         height='333'
