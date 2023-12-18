17c5d89795b52291d48590ec25db073ef6adc03a
cuong.do <quoccuonguit.1998@mail.com>
2023-12-19 02:46:35 +0700

    add class to contact component, contact form in playground

--- playground/src/contact/ContactForm.stories.tsx
+++ playground/src/contact/ContactForm.stories.tsx
@@ -17,8 +17,8 @@ export const Default: Story = () => (
     }}
     className="grid grid-cols-12 gap-4"
   >
-    <div className="col-span-5"></div>
-    <div className="col-span-7">
+    <div className="col-span-5 col-span-12 md:col-span-5"></div>
+    <div className="col-span-7 col-span-12 md:col-span-7">
       <ContactForm />
     </div>
   </div>
