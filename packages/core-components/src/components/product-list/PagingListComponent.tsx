c345a4be59be9ef03a3a6cdf59f65cbbe4cb6a9f
cuong.do <quoccuonguit.1998@mail.com>
2023-12-30 02:53:02 +0700

    wrap Paging Component in PagingList and PagingGrid Component

---
+++ packages/core-components/src/components/product-list/PagingListComponent.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import { PaginationType } from './Pagination';
+import { ProductItemType } from './ProductItem';
+import PagingComponent from './PagingComponent';
+
+type PagingListComponentProps = {
+  productItems: ProductItemType[];
+  paginationPage: PaginationType;
+  previousLabel: string;
+  nextLabel: string;
+  onClickItem?: (id: string) => void;
+  handlePageClick?: (page: number) => void;
+};
+
+const PagingListComponent: React.FC<PagingListComponentProps> = ({
+  productItems,
+  paginationPage,
+  previousLabel,
+  nextLabel,
+  onClickItem,
+  handlePageClick,
+}) => {
+  return (
+    <PagingComponent
+      productItems={productItems}
+      paginationPage={paginationPage}
+      onClickItem={onClickItem}
+      handlePageClick={handlePageClick}
+      classItem={'flex flex-row'}
+      classImg={'basis-2/5 object-contain'}
+      classContent={'basis-3/5'}
+      classPaging={'grid gap-3 grid-cols-1'}
+      previousLabel={previousLabel}
+      nextLabel={nextLabel}
+    />
+  );
+};
+
+export default PagingListComponent;
--- playground/src/product-list/Pagination.stories.tsx
+++ playground/src/product-list/Pagination.stories.tsx
@@ -8,17 +8,43 @@ export default {
   title: '@nv-fe/core-ui/product-list/Pagination',
 } satisfies StoryDefault<typeof Pagination>;
 
+const handlePageClick = (page: number) => {
+  console.log(`Page ${page}`);
+};
 //#F3F3F3 not found
 export const Default: Story = () => (
-  <div className='w-screen bg-[#F3F3F3] p-[20px]'>
-    <div className='mb-[50px]'>
-      <Pagination paginationPage={{ totalPages: 5, currentPage: 4 }} />
+  <div
+    //p-[20px]  doesn't work
+    className='w-screen bg-[#F3F3F3] p-[20px]'
+    style={{ padding: '20px' }}
+  >
+    <div
+      // mb-[50px] doesn't work
+      className='mb-[50px]'
+      style={{ margin: '50px' }}
+    >
+      <Pagination
+        previousLabel={'Trước'}
+        nextLabel={'Sau'}
+        paginationPage={{ totalPages: 5, currentPage: 4 }}
+        handlePageClick={handlePageClick}
+      />
     </div>
-    <div className='mb-[50px]'>
-      <Pagination paginationPage={{ totalPages: 2, currentPage: 1 }} />
+    <div className='mb-[50px]' style={{ margin: '50px' }}>
+      <Pagination
+        previousLabel={'Previous'}
+        nextLabel={'Next'}
+        paginationPage={{ totalPages: 2, currentPage: 1 }}
+        handlePageClick={handlePageClick}
+      />
     </div>
-    <div className='mb-[50px]'>
-      <Pagination paginationPage={{ totalPages: 100, currentPage: 100 }} />
+    <div className='mb-[50px]' style={{ margin: '50px' }}>
+      <Pagination
+        previousLabel={'Trước'}
+        nextLabel={'Sau'}
+        paginationPage={{ totalPages: 100, currentPage: 100 }}
+        handlePageClick={handlePageClick}
+      />
     </div>
   </div>
 );
