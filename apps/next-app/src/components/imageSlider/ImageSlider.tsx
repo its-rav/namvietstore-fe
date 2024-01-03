89e09ef9e4b3829d71c3fed6a282587a4d62b273
phongduong-dp <duonghongphong.dp@gmail.com>
2024-01-04 00:19:05 +0700

    chore: intergrate ImageSlider component

---
+++ apps/next-app/src/components/imageSlider/ImageSlider.tsx
@@ -0,0 +1,11 @@
+import { ImageSlider } from '@namviet-fe/core-ui';
+
+const ImageSliderComponent = () => {
+  return (
+    <div>
+      <ImageSlider />
+    </div>
+  );
+};
+
+export default ImageSliderComponent;
