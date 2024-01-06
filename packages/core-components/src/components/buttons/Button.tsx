7556af5db0604abcc95b5290ef6d299d46973c1c
phongduong-dp <duonghongphong.dp@gmail.com>
2024-01-06 11:41:19 +0700

    fix: button padding to match with design

--- packages/core-components/src/components/buttons/Button.tsx
+++ packages/core-components/src/components/buttons/Button.tsx
@@ -41,7 +41,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
           size === 'sm' && 'px-3 py-3 w-32',
           size === 'md' && 'px-6 py-4 w-36',
           size === 'lg' && 'px-7 py-5 w-40',
-          size === 'fit-content' && 'px-5 py-4 w-fit',
+          size === 'fit-content' && 'px-3 py-3 w-fit',
           loading && 'relative opacity-1 disabled:cursor-wait',
           backgroundColor === 'secondary'
             ? 'bg-secondary hover:bg-secondary-800 active:bg-secondary-700'
