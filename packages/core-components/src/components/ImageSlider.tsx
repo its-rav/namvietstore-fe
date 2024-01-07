ab1d6f655d490b41cff9dcf9b4471c0d42d829f9
phongduong-dp <duonghongphong.dp@gmail.com>
2024-01-07 11:14:26 +0700

    chore: modify image slider

--- packages/core-components/src/components/ImageSlider.tsx
+++ packages/core-components/src/components/ImageSlider.tsx
@@ -2,32 +2,55 @@ import { Carousel } from '@material-tailwind/react';
 import { ThemeProvider } from '@material-tailwind/react';
 import React from 'react';
 
+import clsxm from '@/lib/clsxm';
+
 export type ImageSliderItemType = {
-  url: string;
+  src: string;
   name: string;
+  url: string;
 };
 
 type ImageSliderProps = {
   imageSliderItems: ImageSliderItemType[];
+  imageSliderAutoPlay?: boolean;
+  imageSliderLoop?: boolean;
+  imageSliderAutoplayDelay?: number;
+  imageSliderHeight?: 'sm' | 'md' | 'lg';
 };
 
-const ImageSlider: React.FC<ImageSliderProps> = ({ imageSliderItems }) => {
+const ImageSlider: React.FC<ImageSliderProps> = ({
+  imageSliderItems,
+  imageSliderAutoPlay = true,
+  imageSliderLoop = true,
+  imageSliderAutoplayDelay = 5000,
+  imageSliderHeight = 'md',
+}) => {
   return (
     <ThemeProvider>
-      <Carousel
-        autoplay={true}
-        loop={true}
-        autoplayDelay={2000}
-        className='rounded-xl'
+      <div
+        className={clsxm(
+          'w-full',
+          imageSliderHeight === 'lg' && 'h-96',
+          imageSliderHeight === 'md' && 'h-80',
+          imageSliderHeight === 'sm' && 'h-40'
+        )}
       >
-        {imageSliderItems.map((imageSliderItem) => (
-          <img
-            key={imageSliderItem.name}
-            src={imageSliderItem.url}
-            alt={imageSliderItem.name}
-          />
-        ))}
-      </Carousel>
+        <Carousel
+          autoplay={imageSliderAutoPlay}
+          loop={imageSliderLoop}
+          autoplayDelay={imageSliderAutoplayDelay}
+        >
+          {imageSliderItems.map((imageSliderItem) => (
+            <a key={imageSliderItem.name} href={imageSliderItem.url}>
+              <img
+                src={imageSliderItem.src}
+                alt={imageSliderItem.name}
+                className='h-full w-full object-cover'
+              />
+            </a>
+          ))}
+        </Carousel>
+      </div>
     </ThemeProvider>
   );
 };
--- playground/src/ImageSlider.stories.tsx
+++ playground/src/ImageSlider.stories.tsx
@@ -11,25 +11,32 @@ export default {
 
 const items: ImageSliderItemType[] = [
   {
-    url: 'https://images.unsplash.com/photo-1618221195710-dd6b41faaea6?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8aG9tZSUyMGludGVyaW9yfGVufDB8fDB8fHww',
+    src: 'https://images.unsplash.com/photo-1618221195710-dd6b41faaea6?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8aG9tZSUyMGludGVyaW9yfGVufDB8fDB8fHww',
     name: 'Slide 1',
+    url: '/',
   },
   {
-    url: 'https://www.thespruce.com/thmb/P4hBQtEPZVrrWPdbtXy7-wv9fiE=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-1161177015-f1de4ba58a6c4f50969d9119d80405a6.jpg',
+    src: 'https://www.thespruce.com/thmb/P4hBQtEPZVrrWPdbtXy7-wv9fiE=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-1161177015-f1de4ba58a6c4f50969d9119d80405a6.jpg',
     name: 'Slide 2',
+    url: '/',
   },
   {
-    url: 'https://media.designcafe.com/wp-content/uploads/2023/01/31151510/contemporary-interior-design-ideas-for-your-home.jpg',
+    src: 'https://media.designcafe.com/wp-content/uploads/2023/01/31151510/contemporary-interior-design-ideas-for-your-home.jpg',
     name: 'Slide 3',
+    url: '/',
   },
   {
-    url: 'https://media.designcafe.com/wp-content/uploads/2022/07/29185240/industrial-rustic-living-room-in-earthy-tones.jpg',
+    src: 'https://media.designcafe.com/wp-content/uploads/2022/07/29185240/industrial-rustic-living-room-in-earthy-tones.jpg',
     name: 'Slide 4',
+    url: '/',
   },
   {
-    url: 'https://t3.ftcdn.net/jpg/03/25/31/34/360_F_325313461_0KREdIW6hXqc0eMxfupDnTaJtj2ln6Vd.jpg',
+    src: 'https://t3.ftcdn.net/jpg/03/25/31/34/360_F_325313461_0KREdIW6hXqc0eMxfupDnTaJtj2ln6Vd.jpg',
     name: 'Slide 5',
+    url: '/',
   },
 ];
 
-export const Default: Story = () => <ImageSlider imageSliderItems={items} />;
+export const Default: Story = () => (
+  <ImageSlider imageSliderItems={items} imageSliderHeight='lg' />
+);
