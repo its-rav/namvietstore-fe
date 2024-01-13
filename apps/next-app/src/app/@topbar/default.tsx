f88cec38f108a8a251b3005d4ddabda918c03f05
rav <voasd123@gmail.com>
2024-01-13 23:08:22 +0700

    fix(next rouging error): add default route for footer/topbar parallel routing

---
+++ apps/next-app/src/app/@topbar/default.tsx
@@ -0,0 +1,37 @@
+'use server';
+
+import { DropdownButton, EnIcon, TopBar, ViIcon } from '@namviet-fe/core-ui';
+
+export default async function Topbar() {
+  return (
+    <aside className='fixed top-0 left-0 right-0 header w-full hidden md:block'>
+      <TopBar
+        contact1={{
+          name: 'Mr. Duong',
+          phoneNumber: '+91 9876543210',
+        }}
+        contact2={{
+          name: 'Mr. Huynh',
+          phoneNumber: '+91 9876543210',
+        }}
+        languageButton={
+          <DropdownButton
+            selected='vn'
+            options={[
+              {
+                key: 'en',
+                label: 'English',
+                icon: <EnIcon />,
+              },
+              {
+                key: 'vn',
+                label: 'Tiếng Việt',
+                icon: <ViIcon />,
+              },
+            ]}
+          />
+        }
+      />
+    </aside>
+  );
+}
