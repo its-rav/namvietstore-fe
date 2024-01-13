2440e13c806b259c2a41ff3c689929b9ea4eefea
rav <voasd123@gmail.com>
2024-01-14 00:51:47 +0700

    chore: add api calls for features

--- apps/next-app/src/components/featureList/FeatureList.tsx
+++ apps/next-app/src/components/featureList/FeatureList.tsx
@@ -7,33 +7,62 @@ import {
   UserIcon,
 } from '@namviet-fe/core-ui';
 
-const FeaturesListSection = () => {
-  const items: FeaturesListItemType[] = [
-    {
-      icon: <ProductIcon />,
-      title: 'Sản phẩm',
-      description: 'Đa dạng và chất lượng',
-      id: 1,
-    },
-    {
-      icon: <HeadphonesIcon />,
-      title: 'Dịch vụ',
-      description: 'Uy tín và tận tâm',
-      id: 2,
-    },
-    {
-      icon: <KeyIcon />,
-      title: 'Trách nhiệm',
-      description: 'Đồng hành và phát triển cùng khách hàng',
-      id: 3,
-    },
-    {
-      icon: <UserIcon />,
-      title: 'Nhân viên',
-      description: 'Đội ngũ giàu kinh nghiệm, có chuyên môn',
-      id: 4,
-    },
+import { getFFUseApi } from '@/api/common/env';
+import { fetchFeaturesAsync } from '@/api/home-page';
+
+export const getFeatures = async () => {
+  const useApi = getFFUseApi();
+
+  if (!useApi) {
+    return await Promise.resolve([
+      {
+        icon: <ProductIcon />,
+        title: 'Sản phẩm',
+        description: 'Đa dạng và chất lượng',
+        id: 1,
+      },
+      {
+        icon: <HeadphonesIcon />,
+        title: 'Dịch vụ',
+        description: 'Uy tín và tận tâm',
+        id: 2,
+      },
+      {
+        icon: <KeyIcon />,
+        title: 'Trách nhiệm',
+        description: 'Đồng hành và phát triển cùng khách hàng',
+        id: 3,
+      },
+      {
+        icon: <UserIcon />,
+        title: 'Nhân viên',
+        description: 'Đội ngũ giàu kinh nghiệm, có chuyên môn',
+        id: 4,
+      },
+    ]);
+  }
+
+  const res = await fetchFeaturesAsync('vi-Vn');
+
+  // TODO: add icon for api
+  const iconMap = [
+    <ProductIcon />,
+    <HeadphonesIcon />,
+    <KeyIcon />,
+    <UserIcon />,
   ];
+
+  console.log(res);
+  return res.attributes.Features.map((item, index) => ({
+    id: item.id.toString(),
+    icon: iconMap[index],
+    title: item.name,
+    description: item.description,
+  }));
+};
+
+const FeaturesListSection = async () => {
+  const items: FeaturesListItemType[] = await getFeatures();
 
   return <FeaturesList items={items} />;
 };
