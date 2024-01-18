fdfa8c280ebdbc65a0899203a48b6f39045907ee
Phong Duong <92157056+phongduong-dp@users.noreply.github.com>
2024-01-18 21:48:17 +0700

    Fix/footer responsive (#75)
    * fix: update responsive for footer
    
    * chore: prettier

--- apps/next-app/src/app/@footer/page.tsx
+++ apps/next-app/src/app/@footer/page.tsx
@@ -10,15 +10,15 @@ export default async function Footer() {
 
   const { contact, stamp, map } = footer;
   return (
-    <footer className='py-5 bg-secondary w-full'>
-      <div className='flex max-w-7xl mx-auto items-center'>
+    <footer className='py-5 bg-secondary w-full '>
+      <div className='flex flex-col md:flex-row px-5 max-w-7xl mx-auto items-center'>
         <ContactTable
           contactTableItem={contact}
           stamp={
             <Image src={stamp.src} alt={stamp.alt} width={200} height={200} />
           }
         />
-        <div className='flex h-full w-4/5 border-l border-slate-500 ml-12 pl-12'>
+        <div className='flex h-full w-full lg:w-4/5 lg:border-l lg:border-slate-500 lg:ml-12 lg:pl-12'>
           <div className='w-full'>
             <ContactMap
               contactMapItem={{
