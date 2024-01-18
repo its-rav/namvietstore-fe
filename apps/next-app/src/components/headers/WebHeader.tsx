a7bfe809d21ec1770477e16530bfd010a0a45fef
phongduong-dp <duonghongphong.dp@gmail.com>
2024-01-18 23:44:21 +0700

    chore: intergate for  web header

--- apps/next-app/src/components/headers/WebHeader.tsx
+++ apps/next-app/src/components/headers/WebHeader.tsx
@@ -1,6 +1,7 @@
 'use client';
 import {
   type categoryBarItemType,
+  type CategoryMenuItemType,
   CategoryBar,
   HamburgerIcon,
   KeyIcon,
@@ -35,6 +36,7 @@ const categoryMenuItems: CategoryMenuItemType[] = [
     label: 'Đầu đốt',
     subItems: [
       {
+        key: 'item1-1',
         title: 'Đầu đốt theo hãng',
         items: [
           {
@@ -43,15 +45,16 @@ const categoryMenuItems: CategoryMenuItemType[] = [
           },
           {
             description: 'Madas',
-            url: '/2',
+            url: '/23',
           },
           {
             description: 'Madas',
-            url: '/3',
+            url: '/34',
           },
         ],
       },
       {
+        key: 'item1-2',
         title: 'Đầu đốt theo loại nhiên liệu',
         items: [
           {
@@ -60,15 +63,16 @@ const categoryMenuItems: CategoryMenuItemType[] = [
           },
           {
             description: 'Madas',
-            url: '/2',
+            url: '/25',
           },
           {
             description: 'Madas',
-            url: '/3',
+            url: '/36',
           },
         ],
       },
       {
+        key: 'item1-3',
         title: 'Đầu đốt theo lĩnh vực',
         items: [
           {
@@ -77,11 +81,29 @@ const categoryMenuItems: CategoryMenuItemType[] = [
           },
           {
             description: 'Madas',
-            url: '/2',
+            url: '/27',
           },
           {
             description: 'Madas',
-            url: '/3',
+            url: '/38',
+          },
+        ],
+      },
+      {
+        key: 'item1-4',
+        title: 'Đầu đốt theo lĩnh vực',
+        items: [
+          {
+            description: 'Madas',
+            url: '/madas',
+          },
+          {
+            description: 'Madas',
+            url: '/test',
+          },
+          {
+            description: 'Madas',
+            url: '/091',
           },
         ],
       },
@@ -93,6 +115,43 @@ const categoryMenuItems: CategoryMenuItemType[] = [
     label: 'Đầu đốt',
     subItems: [
       {
+        key: 'item2-1',
+        title: 'Đầu đốt theo hãng',
+        items: [
+          {
+            description: 'Madas',
+            url: '/1',
+          },
+          {
+            description: 'Madas',
+            url: '/2',
+          },
+          {
+            description: 'Madas',
+            url: '/3',
+          },
+        ],
+      },
+      {
+        key: 'item2-2',
+        title: 'Đầu đốt theo hãng',
+        items: [
+          {
+            description: 'Madas',
+            url: '/1',
+          },
+          {
+            description: 'Madas',
+            url: '/2',
+          },
+          {
+            description: 'Madas',
+            url: '/3',
+          },
+        ],
+      },
+      {
+        key: 'item2-3',
         title: 'Đầu đốt theo hãng',
         items: [
           {
@@ -117,6 +176,7 @@ const categoryMenuItems: CategoryMenuItemType[] = [
     label: 'Đầu đốt',
     subItems: [
       {
+        key: 'item3-1',
         title: 'Đầu đốt theo hãng',
         items: [
           {
@@ -141,6 +201,79 @@ const categoryMenuItems: CategoryMenuItemType[] = [
     label: 'Nồi hơi và nồi nước nóng đốt',
     subItems: [
       {
+        key: 'item4-1',
+        title: 'Đầu đốt theo hãng',
+        items: [
+          {
+            description: 'Madas',
+            url: '/12',
+          },
+          {
+            description: 'Madas',
+            url: '/2',
+          },
+          {
+            description: 'Madas',
+            url: '/3',
+          },
+        ],
+      },
+      {
+        key: 'item4-2',
+        title: 'Đầu đốt theo hãng',
+        items: [
+          {
+            description: 'Madas',
+            url: '/12',
+          },
+          {
+            description: 'Madas',
+            url: '/2',
+          },
+          {
+            description: 'Madas',
+            url: '/3',
+          },
+        ],
+      },
+      {
+        key: 'item4-3',
+        title: 'Đầu đốt theo hãng',
+        items: [
+          {
+            description: 'Madas',
+            url: '/12',
+          },
+          {
+            description: 'Madas',
+            url: '/2',
+          },
+          {
+            description: 'Madas',
+            url: '/3',
+          },
+        ],
+      },
+      {
+        key: 'item4-4',
+        title: 'Đầu đốt theo linh vuc nghiep vu',
+        items: [
+          {
+            description: 'Madas',
+            url: '/12',
+          },
+          {
+            description: 'Madas',
+            url: '/2',
+          },
+          {
+            description: 'Madas',
+            url: '/3',
+          },
+        ],
+      },
+      {
+        key: 'item4-5',
         title: 'Đầu đốt theo hãng',
         items: [
           {
@@ -168,6 +301,10 @@ const WebHeader = () => {
     router.push(url);
   };
 
+  const handleCategorySubMenuItemClicked = (url: string) => {
+    router.push(url);
+  };
+
   return (
     <div>
       <div className='w-full h-full border-b-slate-200 border-b-2 border-solid'>
@@ -191,6 +328,7 @@ const WebHeader = () => {
           categoryBarItems={categoryItems}
           categoryMenuItems={categoryMenuItems}
           onCategoryBarItemClick={handleCategoryBarItemClicked}
+          onCategorySubMenuItemClick={handleCategorySubMenuItemClicked}
         />
       </div>
     </div>
